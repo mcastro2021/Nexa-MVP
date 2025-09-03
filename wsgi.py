6ac21243b7525238ac2e135d9a071160b8dfe3e2
mcastro2021 <70715643+mcastro2021@users.noreply.github.com>
2025-09-02 22:38:53 -0300

    start over

--- wsgi.py
+++
@@ -1,8 +0,0 @@
-# WSGI entry point for Render deployment
-from app import app
-
-# This is the WSGI application that Gunicorn will use
-application = app
-
-if __name__ == "__main__":
-    app.run()
